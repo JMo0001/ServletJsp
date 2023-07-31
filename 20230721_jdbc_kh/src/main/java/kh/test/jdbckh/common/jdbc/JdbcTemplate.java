db8115df461df975b5e1b148fe25929ea2efdf15
JM00001 <liser1@naver.com>
2023-07-31 14:24:58 +0900

    properties 이용
    gitignore 변경

--- 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/common/jdbc/JdbcTemplate.java
+++ 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/common/jdbc/JdbcTemplate.java
@@ -1,5 +1,9 @@
 package kh.test.jdbckh.common.jdbc;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,13 +17,25 @@ public class JdbcTemplate {
 	//Singleton 패턴 >> Connection 객체가 많이 생성됨을 방지.
 	public static Connection getConnection() {
 		Properties prop = new Properties();
+		String currentPath = JdbcTemplate.class.getResource("./").getPath();
 		
+		System.out.println("currentPath: "+currentPath);
+//		currentPath: /D:/workspace/github/ServletJsp/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/20230721_jdbc_kh/WEB-INF/classes/kh/test/jdbckh/common/jdbc/
+
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:XE","kh","kh");
+			//driver.properties 파일 로딩함
+			prop.load(new BufferedReader( new FileReader(currentPath+"driver.properties")));
+			//1. driver 있다면 로딩함 // 없다면 ClassnotFoundException
+			Class.forName(prop.getProperty("jdbc.driver"));
+			//2. Connection 객체 생성 // dbms와 연결
+			conn = DriverManager.getConnection(prop.getProperty("jdbc.lurl"),prop.getProperty("jdbc.username"),prop.getProperty("jdbc.password"));
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		if(conn!=null) {
